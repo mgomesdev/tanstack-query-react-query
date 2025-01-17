cbd0e08648d83a477f5294272304d94a645d9056
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-01-17 11:40:07 -0300

    test: adicinando proximos passos

---
+++ __tests__/auto-refetching-polling-realtime/TODO.test.tsx
@@ -0,0 +1,23 @@
+describe("Proximos passos -> auto refetching polling realtime", () => {
+   it("Deve renderizar o titulo", () => {
+      throw "Auto refetching polling realtime";
+   });
+
+   it.todo("Deve renderizar o paragrafo");
+
+   describe("Deve renderizar o label Query Interval speed (ms): corretamente", () => {
+      it.todo("Deve renderizar o title");
+
+      it.todo("Deve renderizar o input number:step");
+
+      it.todo("Deve renderizar o span indicador piscando");
+   });
+
+   it.todo("Deve renderizar o titulo TODO list");
+
+   it.todo("Deve renderizar o form com input corretamente");
+
+   it.todo("Deve renderizar a lista");
+
+   it.todo("Deve renderizar o botao de Clear All");
+});
